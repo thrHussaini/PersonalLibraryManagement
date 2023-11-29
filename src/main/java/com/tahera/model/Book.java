3d4da81835f2078f9a5d3812be5aaa66e65045a3
Hasier Iñan <hasier.inan@zartis.com>
2023-11-29 22:58:33 +0100

    PLM-6 Included minor changes to optimize imports, make code slighlty more readable and make use of constructor injection in the service.

--- src/main/java/com/tahera/model/Book.java
+++ src/main/java/com/tahera/model/Book.java
@@ -1,6 +1,12 @@
 package com.tahera.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(
